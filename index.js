49c055705cd4b62ca049231e26b6288dfe041291
George.c <george.clark0426@gmail.com>
2024-08-01 20:29:30 -0500

    here

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+import { header, nav, footer, main } from "./components";
 // add menu toggle to bars icon in nav bar
-document.querySelector(".fa-bars").addEventListener("click", () => {
-  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-});
+//document.querySelector(".fa-bars").addEventListener("click", () => {
+//  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+//});
+
+function render() {
+  document.querySelector("#root").innerHTML = `
+      ${header()}
+      ${nav()}
+      ${main()}
+      ${footer()}
+    `;
+}
+
+render();
---
+++ store/index.js
@@ -0,0 +1,5 @@
+export { default as aboutMe } from "./aboutMe";
+export { default as nav } from "./nav";
+export { default as pizza } from "./pizza";
+export { default as home } from "./home";
+export { default as order } from "./order";
